ed77b135212ff3a0844fa789c5326edad494dff0
steveoh <sgourley@utah.gov>
2014-08-25 18:38:34 -0600

    copy.js

---
+++ grunt/copy.js
@@ -0,0 +1,10 @@
+        copy: {
+            main: {
+                src: 'src/ChangeLog.html',
+                dest: 'dist/ChangeLog.html'
+            },
+            deploy: {
+                src: 'deploy/some.zip',
+                dest: 'Z:/Documents/some.zip'
+            }
+        }
