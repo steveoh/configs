d764798588b7d48713d4254d943f5acecc3698a5
steveoh <sgourley@utah.gov>
2014-08-25 18:37:57 -0600

    grunt esri slurp

---
+++ grunt/esrl_slurp.js
@@ -0,0 +1,16 @@
+    esri_slurp: {
+      options: {
+        version: '3.10'
+      },
+      dev: {
+        options: {
+          dest: 'src/esri',
+          beautify: true
+        }
+      },
+      travis: {
+        options: {
+          beautify: false
+        }
+      }
+    }
