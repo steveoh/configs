2c09bc16f0a540301c7bd3530ee88858523dc654
steveoh <sgourley@utah.gov>
2014-09-10 15:58:55 -0600

    Create ssh.js

---
+++ grunt/ssh.js
@@ -0,0 +1,63 @@
+grunt-ssh
+
+deployDir = 'FiberAvailabilityMap',
+secrets;
+try {
+    secrets = grunt.file.readJSON('secrets.json');
+} catch (e) {
+// swallow for build server
+    secrets = {
+        stageHost: '',
+        prodHost: '',
+        username: '',
+        password: ''
+    };
+}
+
+secrets: secrets,
+sftp: {
+    stage: {
+        files: {
+            './': 'deploy/deploy.zip'
+        },
+        options: {
+            host: '<%= secrets.stageHost %>'
+        }
+    },
+    prod: {
+        files: {
+            './': 'deploy/deploy.zip'
+        },
+        options: {
+            host: '<%= secrets.prodHost %>'
+        }
+    },
+    options: {
+        path: './' + deployDir + '/',
+        srcBasePath: 'deploy/',
+        username: '<%= secrets.username %>',
+        password: '<%= secrets.password %>',
+        showProgress: true
+    }
+},
+sshexec: {
+    options: {
+        username: '<%= secrets.username %>',
+        password: '<%= secrets.password %>'
+    },
+    stage: {
+        command: ['cd ' + deployDir, 'unzip -o deploy.zip', 'rm deploy.zip'].join(';'),
+        options: {
+            host: '<%= secrets.stageHost %>'
+        }
+    },
+    prod: {
+        command: ['cd ' + deployDir, 'unzip -o deploy.zip', 'rm deploy.zip'].join(';'),
+        options: {
+            host: '<%= secrets.prodHost %>'
+        }
+    }
+}
+
+sftp:stage
+sshexec:stage
