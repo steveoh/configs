4929ad407038f66220e7a952bec2dbe5a9e07b8f
steveoh <sgourley@utah.gov>
2014-09-18 20:53:43 -0600

    version 1 updates

--- grunt/esrl_slurp.js
+++ grunt/esrl_slurp.js
@@ -1,18 +1,21 @@
-    esri_slurp: {
-      options: {
+npm install grunt-esri-slurp --save-dev
+
+esri_slurp: {
+    options: {
         version: '3.10'
-      },
-      dev: {
+    },
+    dev: {
         options: {
-          beautify: true
-        },
-        dest: 'src/esri'
-      },
-      travis: {
-        options: {
-          beautify: false
+            beautify: true,
+            dest: 'src/esri'
         }
-      }
+    },
+    travis: {
+        options: {
+            beautify: false,
+            dest: 'src/esri'
+        }
     }
+}
 
 grunt if-missing:esri_slurp:dev
