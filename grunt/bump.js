bff59f49e5048520cd0ac6e07df092117b823b3a
steveoh <sgourley@utah.gov>
2014-09-25 11:27:09 -0600

    updated workflow

--- grunt/bump.js
+++ grunt/bump.js
@@ -1,14 +1,20 @@
-    var bumpFiles = [
-        'package.json',
-        'bower.json',
-        'src/app/package.json',
-        'src/app/config.js'
-    ];
-    
-        bump: {
-            options: {
-                files: bumpFiles,
-                commitFiles: bumpFiles,
-                push: false
-            }
-        }
+npm install l grunt-bump --save-dev
+
+var bumpFiles = [
+    'package.json',
+    'bower.json',
+    'src/app/package.json',
+    'src/app/config.js'
+];
+
+bump: {
+    options: {
+        files: bumpFiles,
+        commitFiles: bumpFiles.concat('src/ChangeLog.html'),,
+        push: false
+    }
+}
+
+grunt bump-only
+// edit change log etc
+grunt bump-commit
