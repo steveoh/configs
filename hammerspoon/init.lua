ffdce798af0368041dd8056bf3a975c3a73481d0
steveoh <sgourley@utah.gov>
2015-01-22 10:33:21 -0700

    more updates

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -27,36 +27,62 @@ local triple_display = {
     {"ITerm",         nil, display_right, hs.layout.right50,   nil, nil}
 }
 
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "F", function()
+  app = appfinder.appFromName("Wunderlist")
+  win = app:mainWindow()
+  print("x: " .. win:topLeft()["x"])
+  print("y: " .. win:topLeft()["y"])
+  print("width: " .. win:size()["w"])
+  print("height: " .. win:size()["h"])
+end)
+
 if lastNumberOfScreens == 3 then
-  app = appfinder.appFromName("Google Chrome")
+  local app_name = "Google Chrome"
+  hs.application.launchOrFocus(app_name)
+  app = appfinder.appFromName(app_name)
   _win = app:mainWindow()
 
-                            --x, y, w, h
-  winframe = hs.geometry.rect(135, 23, 1155, 877)
+  winframe = hs.geometry.rect(135, 23, 1155, 877) --x, y, w, h
   _win:setFrame(winframe)
 
-  app = appfinder.appFromName("Slack")
+  app_name = "Slack"
+  hs.application.launchOrFocus(app_name)
+  app = appfinder.appFromName(app_name)
   _win = app:mainWindow()
 
   winframe = hs.geometry.rect(-1920, -138, 1024, 1177)
   _win:setFrame(winframe)
 
-  app = appfinder.appFromName("Twitter")
+  app_name = "Twitter"
+  hs.application.launchOrFocus(app_name)
+  app = appfinder.appFromName(app_name)
   _win = app:mainWindow()
 
-  winframe = hs.geometry.rect(-1440, -138, 394, 1177)
+  winframe = hs.geometry.rect(1440, -138, 394, 1177)
   _win:setFrame(winframe)
 
-  app = appfinder.appFromName("LimeChat")
+  app_name = "LimeChat"
+  hs.application.launchOrFocus(app_name)
+  app = appfinder.appFromName(app_name)
   _win = app:mainWindow()
 
   winframe = hs.geometry.rect(2636, 182, 720, 327)
   _win:setFrame(winframe)
 
-  app = appfinder.appFromName("iTerm")
+  app_name = "iTerm"
+  hs.application.launchOrFocus(app_name)
+  app = appfinder.appFromName(app_name)
   _win = app:mainWindow()
 
   winframe = hs.geometry.rect(2717, -138, 640, 315)
+  _win:setFrame(winframe)
+
+  app_name = "Wunderlist"
+  hs.application.launchOrFocus(app_name)
+  app = appfinder.appFromName(app_name)
+  _win = app:mainWindow()
+
+  winframe = hs.geometry.rect(2497, 510, 861, 526)
   _win:setFrame(winframe)
 end
 
