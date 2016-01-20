be14bca5ebc9155da14eb600ed6041f22b36901b
steveoh <sgourley@utah.gov>
2016-01-20 12:36:51 -0700

    update slack position. twitter got bigger.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -5,7 +5,7 @@ local apps = {
   { "Wunderlist", hs.geometry.rect(2497, 510, 861, 526), hs.geometry.rect(795, -529, 864, 526) },
   { "LimeChat", hs.geometry.rect(-895.0, 696.0, 895.0, 344.0), hs.geometry.rect(945, -876, 720, 342), hs.geometry.rect(0.0, 552.0, 895.0, 344.0) },
   { "Twitter", hs.geometry.rect(-895.0, -138.0, 394.0, 834.0), hs.geometry.rect(-256, -1177, 394, 1177), hs.geometry.rect(1045.0, 23.0, 394.0, 873.0) },
-  { "Slack", hs.geometry.rect(-1919.0, -138.0, 1025.0, 1177.0), hs.geometry.rect(140, -1177, 886, 1176), hs.geometry.rect(0.0, 23.0, 1025.0, 873.0) },
+  { "Slack", hs.geometry.rect(-1919.0, -138.0, 1025.0, 1177.0), hs.geometry.rect(161.0, -1177.0, 865.0, 1177.0), hs.geometry.rect(0.0, 23.0, 1025.0, 873.0) },
   { "iTerm", hs.geometry.rect(1440.0, -138.0, 635.0, 337.0), hs.geometry.rect(1027, -1177, 640, 298), hs.geometry.rect(809.0, 23.0, 634.0, 337.0) },
   { "Evernote", hs.geometry.rect(1444, 512, 1048, 524), hs.geometry.rect(-253, -528, 1048, 524) }
   -- { "Adium", },
