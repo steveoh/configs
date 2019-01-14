699db255251ee6938a7069574abb2805c110bf17
steveoh <sgourley@utah.gov>
2019-01-14 09:51:57 -0700

    updates

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,22 +1,34 @@
+hs.application.enableSpotlightForNameSearches(true)
 local appfinder = require("hs.appfinder")
 local apps = {
  -- app name, 3 monitor, 2 monitor, 1 monitor location
-  { "Google Chrome", hs.geometry.rect(134.0, 23.0, 1113.0, 1027.0), hs.geometry.rect(135, 23, 1083, 873), hs.geometry.rect(65.0, 23.0, 981.0, 873.0) },
-  { "Wunderlist", hs.geometry.rect(1256.0, -814.0, 861.0, 525.0), hs.geometry.rect(795, -529, 864, 526) },
-  -- { "LimeChat", hs.geometry.rect(-895.0, 696.0, 895.0, 344.0), hs.geometry.rect(945, -876, 720, 342), hs.geometry.rect(0.0, 552.0, 895.0, 344.0) },
-  { "Twitter", hs.geometry.rect(-443.0, -1417.0, 544.0, 1417.0), hs.geometry.rect(-256, -1177, 394, 1177), hs.geometry.rect(1045.0, 23.0, 394.0, 873.0) },
-  { "Slack", hs.geometry.rect(102.0, -1417.0, 1153.0, 1416.0), hs.geometry.rect(161.0, -1177.0, 865.0, 1177.0), hs.geometry.rect(0.0, 23.0, 1025.0, 873.0) },
-  { "iTerm", hs.geometry.rect(1257.0, -1417.0, 858.0, 331.0), hs.geometry.rect(1027, -1177, 640, 298), hs.geometry.rect(809.0, 23.0, 634.0, 337.0) },
-  { "OneNote", hs.geometry.rect(1257.0, -814.0, 860.0, 525.0), hs.geometry.rect(-253, -528, 1048, 524) },
-  { "Parallels Desktop", hs.geometry.rect(1256.0, -288.0, 861.0, 288.0) }
-  -- { "Adium", },
-  -- { "Messages" },
+  { "Google Chrome", hs.geometry.rect(163.0, 23.0, 1263.0, 1023.0), hs.geometry.rect(135.0, 23.0, 1083.0, 1023.0), hs.geometry.rect(96.0, 23.0, 1381.0, 1022.0) },
+  { "Wunderlist", hs.geometry.rect(-1411.0, -562.0, 864.0, 562.0), hs.geometry.rect(1253.0, -527.0, 864.0, 526.0), hs.geometry.rect(992.0, -562.0, 864.0, 562.0) },
+  { "TweetDeck", hs.geometry.rect(-803.0, -1129.0, 414.0, 1129.0), hs.geometry.rect(-443.0, -1417.0, 414.0, 1417.0), hs.geometry.rect(0.0, 23.0, 414.0, 1021.0) },
+  { "Slack", hs.geometry.rect(-2420.0, -1129.0, 1008.0, 1129.0), hs.geometry.rect(-28.0, -1417.0, 865.0, 1417.0), hs.geometry.rect(-188.0, -1129.0, 991.0, 1129.0) },
+  { "iTerm2", hs.geometry.rect(-1411.0, -1129.0, 810.0, 350.0), hs.geometry.rect(987.0, -1417.0, 1130.0, 293.0), hs.geometry.rect(1094.0, -1129.0, 762.0, 331.0) },
+  { "Microsoft OneNote", hs.geometry.rect(1257.0, -814.0, 860.0, 525.0), hs.geometry.rect(838.0, -782.0, 1052.0, 782.0), hs.geometry.rect(805.0, -634.0, 814.0, 634.0) },
+  { "Parallels Desktop", hs.geometry.rect(-1411.0, -778.0, 810.0, 162.0), hs.geometry.rect(1306.0, -690.0, 810.0, 162.0), hs.geometry.rect(1046.0, -797.0, 810.0, 162.0) },
+  { "Adium", hs.geometry.rect(-2457.0, -1129.0, 133.0, 315.0), hs.geometry.rect(837.0, -1417.0, 153.0, 622.0), hs.geometry.rect(803.0, -1129.0, 153.0, 804.0) },
+  { "Messages", hs.geometry.rect(1457.0, -1123.0, 660.0, 595.0), hs.geometry.rect(1457.0, -1123.0, 660.0, 595.0), hs.geometry.rect(0.0, 23.0, 660.0, 595.0) }
 }
 
 -- refresh config
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
   hs.reload()
   hs.alert.show("Config loaded")
+end)
+
+-- record current position of windows
+hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'T', function ()
+    positions = {}
+    for key,app in pairs(hs.application.runningApplications()) do
+      win = app:mainWindow()
+        if (win ~= nil and win:id() ~= nil and app:title() ~= 'Finder') then
+            print(app:title() .. ": hs.geometry.rect(" .. win:topLeft()["x"] .. ", " .. win:topLeft()["y"] .. ", " .. win:size()["w"] .. ", " .. win:size()["h"] .. ")")
+        end
+    end
+    -- hs.settings.set(workPositions, positions)
 end)
 
 -- open all defaut apps
