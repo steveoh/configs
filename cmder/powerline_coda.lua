eb8dcbc5e9c764d3cfc4e075f900b80ccff4bf6d
steveoh <sgourley@utah.gov>
2019-03-27 10:14:59 -0600

    fix: the env is the full path so shorten it

--- cmder/powerline_coda.lua
+++ cmder/powerline_coda.lua
@@ -7,10 +7,23 @@ local function get_py_env(env_var)
     return false
 end
 
+local function basename(inputstr)
+    sep = "\\/"
+    local last = nil
+    local t={} ; i=1
+
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+        last = str
+    end
+
+    return last
+end
+
 local function get_conda_env_filter()
     local env = get_py_env("CONDA_DEFAULT_ENV")
 
     if env then
+        env = basename(env)
         env = addTextWithColor("", env.." ", ansiFgClrYellow, ansiBgClrBlack)
         clink.prompt.value = string.gsub(clink.prompt.value, plc_prompt_lambSymbol, env..plc_prompt_lambSymbol)
     end
